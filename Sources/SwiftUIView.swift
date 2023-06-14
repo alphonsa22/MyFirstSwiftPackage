519abbf04c4c9e82509f7e73a3b60f2662c2dbb1
alphonsa <alphonsanew22@gmail.com>
2023-06-14 15:41:25 +0530

    updated

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 public struct SwiftUIView: View {
     var str = ""
-  public init() {}
+    public init(str: String? = nil) {
+        self.str = str ?? "No value"
+    }
   public var body: some View {
         Text(str)
     }

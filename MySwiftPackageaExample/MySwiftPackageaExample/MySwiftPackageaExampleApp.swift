ae0221e821a4c99de5586219d80303c60e352ca0
alphonsa <alphonsanew22@gmail.com>
2023-06-14 14:47:21 +0530

    project added

---
+++ MySwiftPackageaExample/MySwiftPackageaExample/MySwiftPackageaExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftPackageaExampleApp.swift
+//  MySwiftPackageaExample
+//
+//  Created by Alphonsa Varghese on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftPackageaExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

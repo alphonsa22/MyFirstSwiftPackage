b3b26af3b97ebe50f16a389aed2b564f094e781f
alphonsa <alphonsanew22@gmail.com>
2023-06-16 10:26:43 +0530

    update

--- Sources/Toaster/Toast.swift
+++ Sources/Toaster/Toast.swift
@@ -11,7 +11,10 @@ import SwiftUI
 public struct ToastView<Content: View>: View {
     @Binding public var isPresented: Bool
   public let content: () -> Content
-    
+    public init(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> Content) {
+        self._isPresented = isPresented
+        self.content = content
+    }
  public var body: some View {
         Group {
             if isPresented {

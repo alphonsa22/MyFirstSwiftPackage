65908b8ac9fd18dadf97002ab43024ea894a487d
alphonsa <alphonsanew22@gmail.com>
2023-06-16 10:59:34 +0530

    update

--- Sources/Toaster/Toast.swift
+++ Sources/Toaster/Toast.swift
@@ -9,32 +9,46 @@ import Foundation
 import SwiftUI
 
 public struct ToastView<Content: View>: View {
-    @Binding public var isPresented: Bool
-  public let content: () -> Content
-    public init(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> Content) {
-        self._isPresented = isPresented
-        self.content = content
-    }
-    public var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                VStack {
-//                    Spacer()
-                    if isPresented {
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(Color.black.opacity(0.7))
-                            .frame(width: 200, height: 100)
-                            .overlay(content())
-                            .transition(.opacity)
-                            .onTapGesture {
-                                withAnimation {
-                                    isPresented = false
-                                }
-                            }
-                    }
-                }
-                .frame(width: geometry.size.width, height: geometry.size.height, alignment: .bottom)
-            }
-        }
-    }
+    @Binding var isPresented: Bool
+     let content: () -> Content
+     let duration: TimeInterval
+     
+     public init(isPresented: Binding<Bool>, duration: TimeInterval, @ViewBuilder content: @escaping () -> Content) {
+         self._isPresented = isPresented
+         self.duration = duration
+         self.content = content
+     }
+     
+     public var body: some View {
+         GeometryReader { geometry in
+             ZStack {
+                 VStack {
+                     Spacer()
+                     if isPresented {
+                         RoundedRectangle(cornerRadius: 10)
+                             .foregroundColor(Color.black.opacity(0.7))
+                             .frame(width: 200, height: 100)
+                             .overlay(content())
+                             .transition(.opacity)
+                             .onAppear {
+                                 startTimer()
+                             }
+                     }
+                 }
+                 .frame(width: geometry.size.width, height: geometry.size.height, alignment: .bottom)
+             }
+         }
+     }
+     
+     private func startTimer() {
+         DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+             dismissToast()
+         }
+     }
+     
+     private func dismissToast() {
+         withAnimation {
+             isPresented = false
+         }
+     }
 }

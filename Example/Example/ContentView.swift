ced23ac2d024ee6fe0dd0ff961053953a79f5bc9
alphonsa <alphonsanew22@gmail.com>
2023-06-16 11:31:36 +0530

    update

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -10,21 +10,43 @@ import MyFirstSwiftPackage
 
 struct ContentView: View {
     @State private var showToast = false
+    @State private var selectedRow: Int?
     var body: some View {
-        VStack {
-//            SwiftUIView(str:"Package Testing....")
-            Button {
-                Log.d("debug testing")
-                showToast.toggle()
-            } label: {
-                Text("Click to log")
+//        VStack {
+////            SwiftUIView(str:"Package Testing....")
+//            Button {
+//                Log.d("debug testing")
+//                showToast.toggle()
+//            } label: {
+//                Text("Click to log")
+//            }
+//            .padding()
+//            ToastView(isPresented: $showToast, duration: 2.0) {
+//                           Text("Toast Message")
+//                               .foregroundColor(.white)
+//                       }
+//        }
+        NavigationView {
+            ZStack {
+                VStack {
+                    List(0..<20) { index in
+                               Text("Cell \(index)")
+                                   .onTapGesture {
+                                       showToast = true
+                                       selectedRow = index
+                                      }
+                           }
+//                    .padding()
+                    
+                    ToastView(isPresented: $showToast, duration: 2.0) {
+                                   Text("selected row is \(selectedRow ?? 0)")
+                                       .foregroundColor(.white)
+                               }
+                    .background(Color.pink)
+                }
+                .navigationTitle("List View")
             }
-            .padding()
-            ToastView(isPresented: $showToast, duration: 2.0) {
-                           Text("Toast Message")
-                               .foregroundColor(.white)
-                       }
-        }
+               }
         
     }
 }

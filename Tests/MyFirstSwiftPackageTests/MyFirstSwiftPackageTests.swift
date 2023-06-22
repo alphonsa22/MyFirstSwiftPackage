aae1e446f25a81e8ac0dae5f77e81f3a8639c456
alphonsa <alphonsanew22@gmail.com>
2023-06-22 13:40:02 +0530

    updated

--- Tests/MyFirstSwiftPackageTests/MyFirstSwiftPackageTests.swift
+++ Tests/MyFirstSwiftPackageTests/MyFirstSwiftPackageTests.swift
@@ -6,6 +6,6 @@ final class MyFirstSwiftPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyFirstSwiftPackage().text, "Hello, World!")
+//        XCTAssertEqual(MyFirstSwiftPackage().text, "Hello, World!")
     }
 }

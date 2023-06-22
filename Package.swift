f58741b6e626603ebda7071305feffbd08a65d61
alphonsa <alphonsanew22@gmail.com>
2023-06-22 13:38:44 +0530

    updated

--- Package.swift
+++ Package.swift
@@ -15,16 +15,17 @@ let package = Package(
             targets: ["MyFirstSwiftPackage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.7.1")
+//        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.7.1")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .product(name: "Alamofire", package: "Alamofire")
         .target(
             name: "MyFirstSwiftPackage",
-            dependencies: ["Alamofire"],
+            dependencies: [],
             path: "Sources",
             sources: ["CoreData"]),
         .testTarget(

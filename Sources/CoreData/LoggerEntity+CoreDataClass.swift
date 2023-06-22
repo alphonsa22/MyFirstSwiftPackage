c2836ec148d0acc36a18754a3fd963d5355875b5
alphonsa <alphonsanew22@gmail.com>
2023-06-22 15:00:50 +0530

    updated

--- Sources/CoreData/LoggerEntity+CoreDataClass.swift
+++ Sources/CoreData/LoggerEntity+CoreDataClass.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-
+@objc(LoggerEntity)
 public class LoggerEntity: NSManagedObject {
     func convertToLoggerEntity() -> LoggerMDL {
         return LoggerMDL(timestamp: self.timestamp, message: self.message)
